6d31255168a83f2f17cb1be6db5bb0249f806812
Rajeshwar Rudra <rajeshwar1993@gmail.com>
2020-03-22 19:06:22 +0530

    static data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+//import axios from "axios";
+import transactionsData from "./staticData";
 import "./styles.css";
 
 export default function App() {
-  return <div className="App" />;
+  const [transactions, updateTransactions] = useState(transactionsData);
+
+  useEffect(() => {
+    // TRIED TO GET DATA FROM API
+    //BUT GOT NETWOR ERROR ALWAYS
+
+    // fetch("http://starlord.hackerearth.com/bankAccount", {
+    //   method: "GET",
+    //   mode: "cors"
+    // })
+    //   .then(data => {
+    //     console.log("here");
+    //     console.log(data);
+    //   })
+    //   //.then(tr => {console.log(tr)})
+    //   .catch(err => {
+    //     console.log("here2");
+    //     console.log(err);
+    //   });
+    console.log(transactions);
+  }, []);
+
+  return <div className="App"> NEW APP</div>;
 }
